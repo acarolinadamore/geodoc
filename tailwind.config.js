c416dcbedb3d5931bf156da0f8a07c596be146df
Ana <adamore@grupoimagetech.com.br>
2025-05-27 14:51:13 -0400

    configurações iniciais

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./public/index.html', './src/**/*.vue', './src/**/*.js'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
